14e63e4a7af6c5f073ebf4bf731ebc571f40be61
SaDiablo <dawd785@gmail.com>
2021-03-21 14:56:32 +0100

    Final net472 version of the app

---
+++ GenshinCompanion/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Drawing;
+using System.Windows;
+
+namespace GenshinWishCalculator
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public App()
+        {
+
+        }
+    }
+}
